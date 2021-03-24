5cd99376247ef06bec8d010b1f5cddf1aaa2bd23
Reti35 <reneribera3@gmail.com>
2021-03-24 13:09:56 +0100

    a

--- CalculadoraFX/src/controlador/CalculadoraController.java
+++ CalculadoraFX/src/controlador/CalculadoraController.java
@@ -1,5 +1,21 @@
 package controlador;
 
-public class CalculadoraController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+//import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class CalculadoraController implements Initializable {
+	
+	@FXML private Button boton_0;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		//boton_0.setOnMouseClicked((event));
+	}
 	
 }
